fcb9528276f51be6b94f6441e91ff5d3b0be4966
Govindaraajan2807 <govindaraajan98@gmail.com>
2022-12-10 20:03:46 +0530

    Update Maze.java
    code for path restrictions

--- Recursion/Code/src/Permutations/Maze.java
+++ Recursion/Code/src/Permutations/Maze.java
@@ -1,10 +1,39 @@
 package Permutations;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
         System.out.println(count(3,3));
         printRoute("",3,3);
+        System.out.println(printRouteList("",3,3));
+
+        boolean[][] board = {
+                {true, true,true},
+                {true,false,true},
+                {true,true,true}
+        };
+        printRouteRestrictions("",0,0,board);
     }
+
+    public static void printRouteRestrictions(String p, int r, int c, boolean[][] maze){
+        if(r == maze.length-1 && c == maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        if(r < maze.length-1){
+            printRouteRestrictions(p+'D',r+1,c,maze);
+        }
+        if(c < maze[0].length-1){
+            printRouteRestrictions(p+'R',r,c+1,maze);
+        }
+    }
+
     public static int count(int r, int c){
         if(r == 1 || c == 1){
             return 1;
@@ -25,5 +54,20 @@ public class Maze {
         if(c > 1){
             printRoute(p+'R',r,c-1);
         }
+    }
+    public static ArrayList printRouteList(String p, int r, int c){
+        if(r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r > 1){
+            list.addAll(printRouteList(p+'D',r-1,c));
+        }
+        if(c > 1){
+            list.addAll(printRouteList(p+'R',r,c-1));
+        }
+        return list;
     }
 }
